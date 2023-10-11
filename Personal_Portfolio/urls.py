71ed74e638ff2f20dd507dabdaa62c76110ce53e
Chirag Bhandakkar <chiraagbhandakkar@gmail.com>
2023-10-12 00:25:15 +0530

    host

--- Personal_Portfolio/urls.py
+++ Personal_Portfolio/urls.py
@@ -19,10 +19,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('blog/', include('blog.urls')),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("blog/", include("blog.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
